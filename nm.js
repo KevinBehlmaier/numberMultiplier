7db8382a85557eb75213b111477213cb2acb2710
KevinBehlmaier <kevin.behlmaier@gmail.com>
2018-03-06 18:14:25 -0700

    Adds second input to program

--- nm.js
+++ nm.js
@@ -1,11 +1,12 @@
-var input = document.getElementById('input')
+var input1 = document.getElementById('input1')
+var input2 = document.getElementById('input2')
 var btn = document.getElementById('btn')
 var text = document.getElementById('text')
 
-function multiplier(input) {
-  return input * 5;
+function multiplier(input1, input2) {
+  return input1 * input2;
 }
 function updateText() {
-  var newNum = multiplier(input.value);
+  var newNum = multiplier(input1.value, input2.value);
   text.innerHTML = "Result: " + newNum;
 }
